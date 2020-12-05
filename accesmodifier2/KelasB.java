0062c95a84028d5c5b32ab8b29535eb53815af5c
KholisEnka22 <enka.run2206@gmail.com>
2020-12-05 11:11:19 +0700

    Access Modifier

---
+++ accesmodifier2/KelasB.java
@@ -0,0 +1,12 @@
+package accesmodifier2;
+
+import accesmodifier1.KelasA;
+
+public class KelasB extends KelasA{
+    
+    @Override
+    protected void methodC() {
+        super.methodC();
+        System.out.println("Contoh pemanggilan protected dari package luar");
+    }
+}
